283be61795f8fc6b71ac8de69fbc3c83ed28cd6f
Marvin <hofmann.marvin03@gmail.com>
2024-09-13 05:15:59 +0200

    more alignment for experiment

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,41 +1,39 @@
 import './Sidebar.css'
 import logo from "./../../assets/icons/logo-t-systems-white.svg";
-import { Link } from 'react-router-dom';
-import { useRef, useState } from 'react';
-import MenuIcon from '../../assets/icons/Menu-Icon';
-import ConfluenceIcon from '../../assets/icons/Confluence-Icon';
-import CrossIcon from '../../assets/icons/Cross-Icon';
-import SettingsIcon from '../../assets/icons/Settings-Icon';
-import ArchitectureIcon from '../../assets/icons/Architecture-Icon';
-import RessourcesIcon from '../../assets/icons/Ressources';
-import ExpertiseIcon from '../../assets/icons/Expertise-Icon';
-import ContactIcon from '../../assets/icons/Contact-Icon';
+import ConfluenceIcon from './../../assets/icons/Confluence-Icon';
+import SettingsIcon from './../../assets/icons/Settings-Icon';
+import ArchitectureIcon from './../../assets/icons/Architecture-Icon';
+import RessourcesIcon from './../../assets/icons/Ressources';
+import ExpertiseIcon from './../../assets/icons/Expertise-Icon';
+import ContactIcon from './../../assets/icons/Contact-Icon';
 
 export default function Sidebar(){
-
-  let [showSidebar, setShowSidebar] = useState(false);
 
   return (
     <>
       <div id="sidebar">
-        <Link to="/" id="logo-wrapper">
-          <img src={logo} alt="" id="logo"/>
+        <a href="#" id="logo-wrapper">
+          <img
+            id="logo"
+            src={logo}
+            alt="Logo"
+          />
           <span>AI-HUB</span>
-        </Link>
+        </a>
         <div id="nav-item-wrapper">
           <p className="info-text">Navigation</p>
-          <Link to="/confluence" className="nav-item">
+          <a href="#" className="nav-item">
             <ConfluenceIcon></ConfluenceIcon>
             <span>Confluence portal</span>
-          </Link>
+          </a>
           <a href="/#use-cases" className="nav-item">
             <SettingsIcon fillColor='white' strokeColor='var(--magenta)'></SettingsIcon>
             <span>Use cases</span>
           </a>
-          <Link to="/architecture" className="nav-item">
+          <a href="#" className="nav-item">
             <ArchitectureIcon></ArchitectureIcon>
             <span>Architecture</span>
-          </Link>
+          </a>
           <a href="/#ressources" className="nav-item">
             <RessourcesIcon></RessourcesIcon>
             <span>Ressources</span>
@@ -51,50 +49,6 @@ export default function Sidebar(){
         </div>
         <p className="bottom-text">Located in Singapore</p>
       </div>
-
-      <>
-        <div id="header-mobile">
-          <Link to="/" id="logo-wrapper">
-            <img src={logo} alt="" id="logo"/>
-            <span>AI-HUB</span>
-          </Link>
-          <div className="menu-button" onClick={()=>{setShowSidebar(!showSidebar)}}>
-            {!showSidebar && <MenuIcon size={25} strokeColor='var(--dark-950)'></MenuIcon>}
-            {showSidebar && <CrossIcon size={25} strokeColor="var(--dark-950)"></CrossIcon>}
-          </div>
-        </div>
-        <div id="sidebar-mobile-overlay" onClick={()=>{setShowSidebar(!showSidebar)}} className={(showSidebar === false ? 'hidden' : 'visible')}></div>
-        <div id="sidebar-mobile" className={(showSidebar === false ? 'hidden' : 'visible')}>
-          <div id="nav-item-wrapper">
-            <p className="info-text">Navigation</p>
-            <Link to="/confluence" className="nav-item">
-              <ConfluenceIcon fillColor="var(--dark-500)"></ConfluenceIcon>
-              <span>Confluence portal</span>
-            </Link>
-            <a href="/#use-cases" className="nav-item">
-              <SettingsIcon></SettingsIcon>
-              <span>Use cases</span>
-            </a>
-            <Link to="/architecture" className="nav-item">
-              <ArchitectureIcon fillColor="var(--dark-500)"></ArchitectureIcon>
-              <span>Architecture</span>
-            </Link>
-            <a href="/#ressources" className="nav-item">
-              <RessourcesIcon fillColorPrimary='var(--dark-500)' fillColorSecondary='var(--dark-950)'></RessourcesIcon>
-              <span>Ressources</span>
-            </a>
-            <a href="/#expertise" className="nav-item">
-              <ExpertiseIcon fillColorPrimary="var(--dark-500)" fillColorSecondary='var(--dark-950)'></ExpertiseIcon>
-              <span>Our expertise</span>
-            </a>
-            <a href="/#contact" className="nav-item">
-              <ContactIcon fillColor="var(--dark-500)"></ContactIcon>
-              <span>Contact</span>
-            </a>
-          </div>
-          <p className="bottom-text">Located in Singapore</p>
-        </div>
-      </>
     </>
   )
 }
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
+import Home from "./Home";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from "./Home/Home"
-import Confluence from './Confluence/Confluence';
-import Login from '../components/Login/Login';
-import ProtectedRoute from '../components/ProtectedRoute/ProtectedRoute';
-import Architecture from './Architecture/Architecture';
 
 const App = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login/>} />
-                <Route path="/confluence" element={<ProtectedRoute><Confluence/></ProtectedRoute>} />
-                <Route path="/architecture" element={<ProtectedRoute><Architecture/></ProtectedRoute>} />
             </Routes>
         </Router>
     );
